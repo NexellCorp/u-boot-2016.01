6a97a7b0456721ff8dfae41ec7875c0a0bf7176d
Hyunseok Jung <hsjung@nexell.co.kr>
2016-02-12 18:58:45 +0900

    armv8/s5p6818/drone: initial board supports
    This patch enables just minimal board functions like serial, timers etc.
    
    Change-Id: I2c2259cf22adab29eb045feb1427c935aa1c095d
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

---
+++ include/configs/s5p6818_arm64_drone.h
@@ -0,0 +1,185 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Hyunseok, Jung <hsjung@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/*-----------------------------------------------------------------------
+ *  u-boot-2016.01
+ */
+#define CONFIG_SYS_LDSCRIPT "arch/arm/cpu/armv8/u-boot.lds"
+
+#define	CONFIG_MACH_S5P6818
+
+/*-----------------------------------------------------------------------
+ *  System memory Configuration
+ */
+
+#define	CONFIG_SYS_TEXT_BASE			0x43C00000
+/* init and run stack pointer */
+#define	CONFIG_SYS_INIT_SP_ADDR			CONFIG_SYS_TEXT_BASE
+
+/* malloc() pool */
+#define	CONFIG_MEM_MALLOC_START			0x44000000
+/* more than 2M for ubifs: MAX 16M */
+#define CONFIG_MEM_MALLOC_LENGTH		32*1024*1024
+
+/* when CONFIG_LCD */
+#define CONFIG_FB_ADDR				0x46000000
+
+/* Download OFFSET */
+#define CONFIG_MEM_LOAD_ADDR			0x48000000
+
+/* AARCH64 */
+#define COUNTER_FREQUENCY			200000000
+#define CPU_RELEASE_ADDR			CONFIG_SYS_INIT_SP_ADDR
+
+/*-----------------------------------------------------------------------
+ *  High Level System Configuration
+ */
+
+/* Not used: not need IRQ/FIQ stuff	*/
+#undef  CONFIG_USE_IRQ
+/* decrementer freq: 1ms ticks */
+#define CONFIG_SYS_HZ				1000
+
+/* board_init_f */
+#define	CONFIG_SYS_SDRAM_BASE			0x40000000
+#define	CONFIG_SYS_SDRAM_SIZE			0x10000000
+
+/* dram 1 bank num */
+#define CONFIG_NR_DRAM_BANKS			1
+
+/* relocate_code and  board_init_r */
+#define	CONFIG_SYS_MALLOC_END			(CONFIG_MEM_MALLOC_START + \
+						 CONFIG_MEM_MALLOC_LENGTH)
+/* board_init_f, more than 2M for ubifs */
+#define CONFIG_SYS_MALLOC_LEN \
+	(CONFIG_MEM_MALLOC_LENGTH - 0x8000)
+
+/* kernel load address */
+#define CONFIG_SYS_LOAD_ADDR			CONFIG_MEM_LOAD_ADDR
+
+/* memtest works on */
+#define CONFIG_SYS_MEMTEST_START		CONFIG_SYS_MALLOC_END
+#define CONFIG_SYS_MEMTEST_END			((ulong)CONFIG_SYS_SDRAM_BASE \
+						 + (ulong)CONFIG_SYS_SDRAM_SIZE)
+
+/*-----------------------------------------------------------------------
+ *  System initialize options (board_init_f)
+ */
+
+/* board_init_f->init_sequence, call arch_cpu_init */
+#define CONFIG_ARCH_CPU_INIT
+/* board_init_f->init_sequence, call board_early_init_f */
+#define	CONFIG_BOARD_EARLY_INIT_F
+/* board_init_r, call board_early_init_f */
+#define	CONFIG_BOARD_LATE_INIT
+/* board_init_f->init_sequence, call print_cpuinfo */
+#define	CONFIG_DISPLAY_CPUINFO
+/* board_init_f, CONFIG_SYS_ICACHE_OFF */
+#define	CONFIG_SYS_DCACHE_OFF
+/* board_init_r, call arch_misc_init */
+#define	CONFIG_ARCH_MISC_INIT
+/*#define	CONFIG_SYS_ICACHE_OFF*/
+
+/*-----------------------------------------------------------------------
+ *	U-Boot default cmd
+ */
+#define	CONFIG_CMD_MEMTEST
+
+/*-----------------------------------------------------------------------
+ *	U-Boot Environments
+ */
+/* refer to common/env_common.c	*/
+#define CONFIG_BOOTDELAY			3
+
+/*-----------------------------------------------------------------------
+ * Miscellaneous configurable options
+ */
+#ifdef CONFIG_SYS_PROMPT
+#undef CONFIG_SYS_PROMPT
+/* Monitor Command Prompt   */
+#define CONFIG_SYS_PROMPT			"s5p6818# "
+#endif
+/* undef to save memory	   */
+#define CONFIG_SYS_LONGHELP
+/* Console I/O Buffer Size  */
+#define CONFIG_SYS_CBSIZE			1024
+/* Print Buffer Size */
+#define CONFIG_SYS_PBSIZE			(CONFIG_SYS_CBSIZE + \
+						 sizeof(CONFIG_SYS_PROMPT)+16)
+/* max number of command args   */
+#define CONFIG_SYS_MAXARGS			16
+/* Boot Argument Buffer Size    */
+#define CONFIG_SYS_BARGSIZE			CONFIG_SYS_CBSIZE
+
+/*-----------------------------------------------------------------------
+ * allow to overwrite serial and ethaddr
+ */
+#define CONFIG_ENV_OVERWRITE
+#define CONFIG_SYS_HUSH_PARSER			/* use "hush" command parser */
+#ifdef CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
+#endif
+
+/*-----------------------------------------------------------------------
+ * Etc Command definition
+ */
+#define	CONFIG_CMD_IMI				/* image info	*/
+#define CONFIG_CMDLINE_EDITING			/* add command line history */
+#define	CONFIG_CMDLINE_TAG			/* use bootargs commandline */
+#undef	CONFIG_BOOTM_NETBSD
+#undef	CONFIG_BOOTM_RTEMS
+
+/*-----------------------------------------------------------------------
+ * serial console configuration
+ */
+#define CONFIG_S5P_SERIAL
+#define CONFIG_S5P_SERIAL_INDEX			0
+#define CONFIG_S5P_SERIAL_CLOCK			50000000
+#define CONFIG_S5P_SERIAL_PORT			(void *)PHY_BASEADDR_UART0
+
+#define CONFIG_BAUDRATE				115200
+#define CONFIG_SYS_BAUDRATE_TABLE \
+		{ 9600, 19200, 38400, 57600, 115200 }
+#define CONFIG_S5P_SERIAL_FLUSH_ON_INIT
+
+#define CONFIG_UART_CLKGEN_CLOCK_HZ		CONFIG_S5P_SERIAL_CLOCK
+
+/*-----------------------------------------------------------------------
+ * NOR FLASH
+ */
+#define	CONFIG_SYS_NO_FLASH
+
+/*-----------------------------------------------------------------------
+ * Default environment organization
+ */
+#if !defined(CONFIG_ENV_IS_IN_MMC) && !defined(CONFIG_ENV_IS_IN_NAND) && \
+	!defined(CONFIG_ENV_IS_IN_FLASH) && !defined(CONFIG_ENV_IS_IN_EEPROM)
+	/* default: CONFIG_ENV_IS_NOWHERE */
+	#define CONFIG_ENV_IS_NOWHERE
+	#define	CONFIG_ENV_OFFSET		1024
+	#define CONFIG_ENV_SIZE			4*1024		/* env size */
+	/* imls - list all images found in flash, default enable so disable */
+	#undef	CONFIG_CMD_IMLS
+#endif
+
+/*-----------------------------------------------------------------------
+ * PLL
+ */
+
+#define CONFIG_SYS_PLLFIN			24000000UL
+
+/*-----------------------------------------------------------------------
+ * Timer
+ */
+
+#define CONFIG_TIMER_SYS_TICK_CH		0
+
+#endif /* __CONFIG_H__ */
+
