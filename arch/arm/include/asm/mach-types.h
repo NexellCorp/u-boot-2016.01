322bfd09234f38bd34ad264ed42670ab5b743a86
Hyunseok Jung <hsjung@nexell.co.kr>
2016-02-12 18:38:26 +0900

    machtype : add s5p6818 mach type number
    Change-Id: I938400367a2e891d43daa84e2eda2d7d35f398ab
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -1107,6 +1107,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_COLIBRI_T30          4493
 #define MACH_TYPE_APALIS_T30           4513
 #define MACH_TYPE_OMAPL138_LCDK        2495
+#define MACH_TYPE_S5P6818	       6818
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
@@ -14186,6 +14187,18 @@ extern unsigned int __machine_arch_type;
 # define machine_is_apalis_t30()	(machine_arch_type == MACH_TYPE_APALIS_T30)
 #else
 # define machine_is_apalis_t30()	(0)
+#endif
+
+#ifdef CONFIG_MACH_S5P6818
+# ifdef machine_arch_type
+#  undef  machine_arch_type
+#  define machine_arch_type	__machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_S5P6818
+# endif
+# define machine_is_s5p6818()		(machine_arch_type == MACH_TYPE_S5P6818)
+#else
+# define machine_is_s5p6818()		(0)
 #endif
 
 /*
