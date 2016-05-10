a38d7d8c74f30774f17f0a1307ae9f2bedbd2bd9
jhkim <jhkim@nexell.co.kr>
2016-05-10 10:22:33 +0900

    display: video: Fix up compile error caused by header file renaming
    This patch fixes up compile error in nexell display driver caused by
    renaming s5p6818.h to nexell.h.
    
    Change-Id: I7c62135aa8572b9ebb159423312c353450a4d7d0
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- drivers/video/nexell/s5pxx18_dp_mipi.c
+++ drivers/video/nexell/s5pxx18_dp_mipi.c
@@ -9,7 +9,7 @@
 #include <common.h>
 #include <errno.h>
 
-#include <asm/arch/s5p6818.h>
+#include <asm/arch/nexell.h>
 #include <asm/arch/tieoff.h>
 #include <asm/arch/reset.h>
 #include <asm/arch/display.h>
