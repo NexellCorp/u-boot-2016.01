NexellCorp/u-boot-2016.01

commit b380538a94704ee099f3e576d1e8ff8ed2a725a3

artik710_raptor: configs: support fdtfile to override dtb file
This patch changes fdt loading mechanism to override dtb
file name. If a user want to set specific dtb name, he/she
can use $fdtfile variable and if $fdtfile variable is not
set, the fdt file can be loading by hush shell script.

Change-Id: I7e818f392db01da230f31ba5eea0b2b6ebb7cb81
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

include/configs/artik710_raptor.h
@@ -309,7 +309,9 @@
 	"ramdiskaddr=0x49000000\0"					\
 	"ramdisk_file=ramdisk.gz\0"					\
 	"fdtaddr=0x4a000000\0"						\
-	"load_fdt="							\
+	"fdtfile=\0"							\
+	"get_fdt_file="							\
+		"if test -z \"$fdtfile\"; then "			\
 		"loop=$board_rev; "					\
 		"number=$board_rev: "					\
 		"success=0; "						\
@@ -318,13 +320,15 @@
 				"number=0$loop; "			\
 			"else number=$loop; "				\
 			"fi; "						\
-			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor-rev${number}.dtb && setexpr success 1; " \
+			"setenv fdtfile s5p6818-artik710-raptor-rev${number}.dtb && setexpr success 1; " \
 			"setexpr loop $loop - 1; "			\
 			"done; "					\
 		"if test $success -eq 0; then "				\
-			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor-rev00.dtb || "	\
-			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor.dtb; "	\
-		"fi; \0"						\
+			"setenv fdtfile s5p6818-artik710-raptor-rev00.dtb || "	\
+			"setenv fdtfile s5p6818-artik710-raptor.dtb; "	\
+		"fi; fi; \0"						\
+	"load_fdt=run get_fdt_file; "					\
+		"ext4load mmc $rootdev:$bootpart $fdtaddr $fdtfile\0"	\
 	"console=" CONFIG_DEFAULT_CONSOLE				\
 	"consoleon=set console=" CONFIG_DEFAULT_CONSOLE			\
 		"; saveenv; reset\0"					\

commit d21a060a189bcd731056faeced59ee4d80543e19

artik710_raptor: configs: correct dtb revisision loading
Fixes: b380538a9470 ("artik710_raptor: configs: support fdtfile to override dtb file")

This patch fixes dtb revision problem since merged b38053.
The revision number can be bigger than supported revision,
so there is no way to check the nearest revision dtb before
trying to load the dtbs.

Change-Id: I5e60512c60f391f61cc57c9918c6416152ef6cce
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

include/configs/artik710_raptor.h
@@ -310,7 +310,7 @@
 	"ramdisk_file=ramdisk.gz\0"					\
 	"fdtaddr=0x4a000000\0"						\
 	"fdtfile=\0"							\
-	"get_fdt_file="							\
+	"load_fdt="							\
 		"if test -z \"$fdtfile\"; then "			\
 		"loop=$board_rev; "					\
 		"number=$board_rev: "					\
@@ -320,15 +320,15 @@
 				"number=0$loop; "			\
 			"else number=$loop; "				\
 			"fi; "						\
-			"setenv fdtfile s5p6818-artik710-raptor-rev${number}.dtb && setexpr success 1; " \
+			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor-rev${number}.dtb && setexpr success 1; " \
 			"setexpr loop $loop - 1; "			\
 			"done; "					\
 		"if test $success -eq 0; then "				\
-			"setenv fdtfile s5p6818-artik710-raptor-rev00.dtb || "	\
-			"setenv fdtfile s5p6818-artik710-raptor.dtb; "	\
-		"fi; fi; \0"						\
-	"load_fdt=run get_fdt_file; "					\
-		"ext4load mmc $rootdev:$bootpart $fdtaddr $fdtfile\0"	\
+			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor-rev00.dtb || "	\
+			"ext4load mmc $rootdev:$bootpart $fdtaddr s5p6818-artik710-raptor.dtb; "	\
+		"fi; "							\
+		"else ext4load mmc $rootdev:$bootpart $fdtaddr $fdtfile; "	\
+		"fi;\0"							\
 	"console=" CONFIG_DEFAULT_CONSOLE				\
 	"consoleon=set console=" CONFIG_DEFAULT_CONSOLE			\
 		"; saveenv; reset\0"					\