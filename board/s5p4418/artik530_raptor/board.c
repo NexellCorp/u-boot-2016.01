1dff5235ef334b2371a21fcfc7bda1cad0ef5f5d
Hyunseok Jung <hsjung@nexell.co.kr>
2016-08-16 17:43:21 +0900

    artik530_raptor: remove ehci vbus gpio control
    This patch remove ehci vbus gpio control
    because current code support gpiolib, dts "samsung,vbus-gpio".
    
    Change-Id: Iad8c2ef4f3245891dc17fead57df7d03913da21b
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

--- board/s5p4418/artik530_raptor/board.c
+++ board/s5p4418/artik530_raptor/board.c
@@ -191,16 +191,6 @@ void serial_clock_init(void)
 	clk_enable(clk);
 }
 
-#ifdef CONFIG_USB_EHCI_EXYNOS
-void board_ehci_power_en(void)
-{
-	/* ehci host power */
-	nx_gpio_set_pad_function(0, 16, 0);     /* GPIO */
-	nx_gpio_set_output_value(0, 16, 1);
-	nx_gpio_set_output_enable(0, 16, 1);
-}
-#endif
-
 /* call from u-boot */
 int board_early_init_f(void)
 {
@@ -252,9 +242,6 @@ int board_init(void)
 #endif
 
 	nx_phy_init();
-#ifdef CONFIG_USB_EHCI_EXYNOS
-	board_ehci_power_en();
-#endif
 
 	l2_cache_en();
 
