07e2c724004a64fde1ce83b7fddd46a13a2e2dec
Hyunseok Jung <hsjung@nexell.co.kr>
2016-08-29 13:22:23 +0900

    usb: dwc-otg: fix build error when DM_USB is not used
    This patch fix build error when DM_USB is not used.
    
    Change-Id: Ief93ff54c11df9d8a5433e741a4e3d0b2f8c3c3a
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

--- drivers/usb/gadget/dwc2_udc_otg.c
+++ drivers/usb/gadget/dwc2_udc_otg.c
@@ -885,7 +885,7 @@ int dwc2_udc_probe(struct dwc2_plat_otg_data *pdata)
 	usb_ctrl_dma_addr = (dma_addr_t) usb_ctrl;
 
 	udc_reinit(dev);
-
+}
 #else
 static int dwc2_udc_probe(struct udevice *dev)
 {
