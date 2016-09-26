ecaa778da0cbc6cde7548ec7ef7cecc05e92bfc9
jhkim <jhkim@nexell.co.kr>
2016-09-26 13:34:35 +0900

    s5p4418: navi_ref: simplify navi_ref's config components and Support SPL
    This patch simplify navi_ref's config components and Support SPL build
    
    Change-Id: I4d279ffa6b24deaaac3556dca1c41bfa61e25911
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- include/configs/s5p4418_navi_ref.h
+++ include/configs/s5p4418_navi_ref.h
@@ -16,6 +16,13 @@
 #define CONFIG_SYS_LDSCRIPT "arch/arm/cpu/armv7/s5p4418/u-boot.lds"
 
 #define	CONFIG_MACH_S5P4418
+#define CONFIG_SYS_THUMB_BUILD
+#define	CONFIG_SKIP_LOWLEVEL_INIT
+
+/*-----------------------------------------------------------------------
+ * PLL
+ */
+#define CONFIG_SYS_PLLFIN			24000000UL
 
 /*-----------------------------------------------------------------------
  *  System memory Configuration
@@ -29,9 +36,6 @@
 #define	CONFIG_MEM_MALLOC_START			0x44000000
 /* more than 2M for ubifs: MAX 16M */
 #define CONFIG_MEM_MALLOC_LENGTH		(32*1024*1024)
-
-/* when CONFIG_LCD */
-#define CONFIG_FB_ADDR				0x46000000
 
 /* Download OFFSET */
 #define CONFIG_MEM_LOAD_ADDR			0x48000000
@@ -49,7 +53,7 @@
 
 /* board_init_f */
 #define	CONFIG_SYS_SDRAM_BASE			0x40000000
-#define	CONFIG_SYS_SDRAM_SIZE			0x40000000
+#define	CONFIG_SYS_SDRAM_SIZE			0x20000000
 
 /* dram 1 bank num */
 #define CONFIG_NR_DRAM_BANKS			1
@@ -64,39 +68,28 @@
 /* kernel load address */
 #define CONFIG_SYS_LOAD_ADDR			CONFIG_MEM_LOAD_ADDR
 
-/* memtest works on */
-#define CONFIG_SYS_MEMTEST_START		CONFIG_SYS_MALLOC_END
-#define CONFIG_SYS_MEMTEST_END			((ulong)CONFIG_SYS_SDRAM_BASE \
-						 + (ulong)CONFIG_SYS_SDRAM_SIZE)
-
 /*-----------------------------------------------------------------------
  *  System initialize options (board_init_f)
+ *  if disabel mmu, set CONFIG_SYS_DCACHE_OFF
  */
 
 /* board_init_f->init_sequence, call arch_cpu_init */
 #define CONFIG_ARCH_CPU_INIT
-/* board_init_f->init_sequence, call board_early_init_f */
-#define	CONFIG_BOARD_EARLY_INIT_F
+
 /* board_init_r, call board_early_init_f */
 #define	CONFIG_BOARD_LATE_INIT
+
 /* board_init_f->init_sequence, call print_cpuinfo */
 #define	CONFIG_DISPLAY_CPUINFO
-/* board_init_f, CONFIG_SYS_ICACHE_OFF */
-#define	CONFIG_SYS_DCACHE_OFF
-/* board_init_r, call arch_misc_init */
-#define	CONFIG_ARCH_MISC_INIT
-/*#define	CONFIG_SYS_ICACHE_OFF*/
 
 /*-----------------------------------------------------------------------
  *	U-Boot default cmd
  */
-#define	CONFIG_CMD_MEMTEST
-
-/*-----------------------------------------------------------------------
- *	U-Boot Environments
- */
-/* refer to common/env_common.c	*/
-#define CONFIG_BOOTDELAY			3
+#ifdef CONFIG_CMD_MEMTEST
+#define CONFIG_SYS_MEMTEST_START		CONFIG_SYS_MALLOC_END
+#define CONFIG_SYS_MEMTEST_END			((ulong)CONFIG_SYS_SDRAM_BASE \
+						 + (ulong)CONFIG_SYS_SDRAM_SIZE)
+#endif
 
 /*-----------------------------------------------------------------------
  * Miscellaneous configurable options
@@ -119,6 +112,17 @@
 #define CONFIG_SYS_BARGSIZE			CONFIG_SYS_CBSIZE
 
 /*-----------------------------------------------------------------------
+ * Default Boot Environments
+ */
+#define CONFIG_BOOTDELAY			1
+#define CONFIG_ZERO_BOOTDELAY_CHECK
+#define CONFIG_SILENT_CONSOLE
+
+#define CONFIG_BOOTCOMMAND	"ext4load mmc 0:1 0x40008000 Image;"	\
+				"ext4load mmc 0:1 49000000 linux.dtb;"	\
+				"bootl 0x40008000 - 49000000"
+
+/*-----------------------------------------------------------------------
  * allow to overwrite serial and ethaddr
  */
 #define CONFIG_ENV_OVERWRITE
@@ -133,8 +137,12 @@
 #define	CONFIG_CMD_IMI				/* image info	*/
 #define CONFIG_CMDLINE_EDITING			/* add command line history */
 #define	CONFIG_CMDLINE_TAG			/* use bootargs commandline */
-#undef	CONFIG_BOOTM_NETBSD
-#undef	CONFIG_BOOTM_RTEMS
+#define	CONFIG_CMD_BOOTL			/* boot linux image */
+
+#undef  CONFIG_BOOTM_NETBSD
+#undef  CONFIG_BOOTM_PLAN9
+#undef  CONFIG_BOOTM_RTEMS
+#undef  CONFIG_BOOTM_VXWORKS
 
 /*-----------------------------------------------------------------------
  * serial console configuration
@@ -158,105 +166,80 @@
 #define	CONFIG_SYS_NO_FLASH
 
 /*-----------------------------------------------------------------------
- * PLL
- */
-
-#define CONFIG_SYS_PLLFIN			24000000UL
-
-/*-----------------------------------------------------------------------
  * Timer
  */
-
 #define CONFIG_TIMER_SYS_TICK_CH		0
-
-/*-----------------------------------------------------------------------
- * PWM
- */
-#define CONFIG_PWM_NX
-
-/*-----------------------------------------------------------------------
- * BACKLIGHT
- */
-#define CONFIG_BACKLIGHT_CH			0
-#define CONFIG_BACKLIGHT_DIV			0
-#define CONFIG_BACKLIGHT_INV			0
-#define CONFIG_BACKLIGHT_DUTY			50
-#define CONFIG_BACKLIGHT_HZ			1000
 
 /*-----------------------------------------------------------------------
  * SD/MMC
  */
-
 #define CONFIG_GENERIC_MMC
 #define CONFIG_MMC
 #define CONFIG_DWMMC
 #define CONFIG_NEXELL_DWMMC
 #define CONFIG_BOUNCE_BUFFER
-#define CONFIG_CMD_MMC
 
-#if defined(CONFIG_MMC)
+#ifdef CONFIG_MMC
 #define CONFIG_2NDBOOT_OFFSET		512
 #define CONFIG_2NDBOOT_SIZE		(64*1024)
 #define CONFIG_FIP_OFFSET		(CONFIG_2NDBOOT_OFFSET +\
 					 CONFIG_2NDBOOT_SIZE)
 #define CONFIG_FIP_SIZE			(3*1024*1024)
+
 #define CONFIG_ENV_IS_IN_MMC
+
+#ifdef CONFIG_ENV_IS_IN_MMC
 #define CONFIG_SYS_MMC_ENV_DEV		0
 #define	CONFIG_ENV_OFFSET		(CONFIG_FIP_OFFSET +\
 					 CONFIG_FIP_SIZE)
-#define CONFIG_ENV_SIZE			(4*1024)	/* env size */
+#define CONFIG_ENV_SIZE			(512)	/* env size */
 #endif
 
-#if defined(CONFIG_MMC)
-#define CONFIG_DOS_PARTITION
-#define CONFIG_CMD_FAT
-#define CONFIG_FS_FAT
-#define CONFIG_FAT_WRITE
+#ifndef CONFIG_FIT
+/* refer to struct nx_dwmci_dat at nexell_dw_mmc.c */
+#define	CONFIG_NEXELL_DWMMC_PLATFORM_DATA {	\
+	[0] = { \
+		.dev = 0, \
+		.frequency = 50000000, \
+		.buswidth = 4,	\
+		.d_delay = 0x0, \
+		.d_shift = 0x03, \
+		.s_delay = 0x00, \
+		.s_shift = 0x02,	\
+	}, \
+}
+#endif
+#endif /* CONFIG_MMC */
 
+#if defined(CONFIG_MMC)
+#define CONFIG_CMD_MMC
+#define CONFIG_DOS_PARTITION
 #define CONFIG_CMD_EXT4
 #define CONFIG_CMD_EXT4_WRITE
 #define CONFIG_FS_EXT4
 #define CONFIG_EXT4_WRITE
+#define CONFIG_CMD_FDISK
+#define	CONFIG_CMD_EXT4_IMG_WRITE
 #endif
-
-/*-----------------------------------------------------------------------
- * Default environment organization
- */
-#if !defined(CONFIG_ENV_IS_IN_MMC) && !defined(CONFIG_ENV_IS_IN_NAND) && \
-	!defined(CONFIG_ENV_IS_IN_FLASH) && !defined(CONFIG_ENV_IS_IN_EEPROM)
-	/* default: CONFIG_ENV_IS_NOWHERE */
-	#define CONFIG_ENV_IS_NOWHERE
-	#define	CONFIG_ENV_OFFSET		1024
-	#define CONFIG_ENV_SIZE			(4*1024)	/* env size */
-	/* imls - list all images found in flash, default enable so disable */
-	#undef	CONFIG_CMD_IMLS
-#endif
-
-/*-----------------------------------------------------------------------
- * GPT
- */
-#define CONFIG_CMD_GPT
-#define CONFIG_EFI_PARTITION
-#define CONFIG_PARTITION_UUIDS
-#define CONFIG_RANDOM_UUID
 
 /*-----------------------------------------------------------------------
  * Fastboot and USB OTG
  */
-
 #define CONFIG_USB_FUNCTION_FASTBOOT
 #define CONFIG_CMD_FASTBOOT
-#define CONFIG_FASTBOOT_FLASH
-#define CONFIG_FASTBOOT_FLASH_MMC_DEV   0
+
 #define CONFIG_FASTBOOT_BUF_SIZE        (CONFIG_SYS_SDRAM_SIZE - SZ_1M)
 #define CONFIG_FASTBOOT_BUF_ADDR        CONFIG_SYS_SDRAM_BASE
+
 #define CONFIG_USB_GADGET
 #define CONFIG_USB_GADGET_DUALSPEED
 #define CONFIG_USB_GADGET_VBUS_DRAW     0
 #define CONFIG_USB_GADGET_DWC2_OTG
 #define CONFIG_USB_GADGET_NX_UDC_OTG_PHY
-#define CONFIG_USB_GADGET_DOWNLOAD
+
 #define CONFIG_SYS_CACHELINE_SIZE       64
+
+#define CONFIG_USB_GADGET_DOWNLOAD
 #define CONFIG_G_DNL_VENDOR_NUM         0x18d1  /* google */
 #define CONFIG_G_DNL_PRODUCT_NUM        0x0002  /* nexus one */
 #define CONFIG_G_DNL_MANUFACTURER       "Nexell Corporation"
@@ -267,16 +250,36 @@
 #define CONFIG_NX_USBDOWN
 
 /*-----------------------------------------------------------------------
- * OF_CONTROL
+ * Default environment organization
  */
-
-#define CONFIG_FIT_BEST_MATCH
-#define CONFIG_OF_LIBFDT
+#if !defined(CONFIG_ENV_IS_IN_MMC) && !defined(CONFIG_ENV_IS_IN_NAND) && \
+	!defined(CONFIG_ENV_IS_IN_FLASH) && !defined(CONFIG_ENV_IS_IN_EEPROM)
+	#define CONFIG_ENV_IS_NOWHERE
+	#define	CONFIG_ENV_OFFSET		1024
+	#define CONFIG_ENV_SIZE			(4*1024)	/* env size */
+	#undef	CONFIG_CMD_IMLS
+#endif
 
 /*-----------------------------------------------------------------------
- * ENV
+ * SPL
  */
-#define CONFIG_EXTRA_ENV_SETTINGS	\
-			"fdt_high=0xffffffff"
+#ifdef CONFIG_SPL
+/* SPL FRAMEWORK */
+#if 1
+#define	CONFIG_SPL_CLI_FRAMEWORK	/* set prompt mode */
+#endif
 
+/* Support Devices */
+#define	CONFIG_SPL_SERIAL_SUPPORT
+#define	CONFIG_SPL_MMC_SUPPORT
+#define CONFIG_NX_USBDOWN
+
+/*
+ * Support commands
+ */
+#define CONFIG_CMD_EXT4
+
+#include <configs/s5p4418_spl.h>
+
+#endif /* CONFIG_SPL */
 #endif /* __CONFIG_H__ */
