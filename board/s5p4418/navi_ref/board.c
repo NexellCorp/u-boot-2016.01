f2d157450a730ef81696786fb7b402d129fc78c7
jhkim <jhkim@nexell.co.kr>
2016-09-26 13:32:05 +0900

    s5p4418 navi_ref: simplify Kconfig and board.c functions
    This patch simplify board.c functions and Kconfig
    
    Change-Id: Iba44c1550932833462cfc4fd33e57e79005708da
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- board/s5p4418/navi_ref/board.c
+++ board/s5p4418/navi_ref/board.c
@@ -7,9 +7,6 @@
 
 #include <config.h>
 #include <common.h>
-#ifdef CONFIG_PWM_NX
-#include <pwm.h>
-#endif
 #include <asm/io.h>
 
 #include <asm/arch/nexell.h>
@@ -17,85 +14,45 @@
 #include <asm/arch/reset.h>
 #include <asm/arch/nx_gpio.h>
 
-#ifdef CONFIG_DM_PMIC_NXE2000
-#include <dm.h>
-#include <dm/uclass-internal.h>
-#include <power/pmic.h>
-#include <power/nxe2000.h>
-#endif
+#include <usb/dwc2_udc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-/*------------------------------------------------------------------------------
- * intialize nexell soc and board status.
- */
+#if !defined CONFIG_OF_CONTROL && !defined CONFIG_DM_USB &&	\
+	 defined CONFIG_USB_GADGET
+static struct dwc2_plat_otg_data nx_otg_data = {
+	.regs_phy = 0xc0011000,
+	.regs_otg = 0xc0040000,
+};
 
-void serial_clock_init(void)
+static void board_udc_probe(void)
 {
-	char dev[10];
-	int id;
-
-	sprintf(dev, "nx-uart.%d", CONFIG_CONS_INDEX);
-	id = RESET_ID_UART0 + CONFIG_CONS_INDEX;
-
-	struct clk *clk = clk_get((const char *)dev);
-
-	/* reset control: Low active ___|---   */
-	nx_rstcon_setrst(id, RSTCON_ASSERT);
-	udelay(10);
-	nx_rstcon_setrst(id, RSTCON_NEGATE);
-	udelay(10);
-
-	/* set clock   */
-	clk_disable(clk);
-	clk_set_rate(clk, CONFIG_PL011_CLOCK);
-	clk_enable(clk);
-}
-
-/* call from u-boot */
-int board_early_init_f(void)
-{
-	serial_clock_init();
-	return 0;
-}
-
-void board_gpio_init(void)
-{
-	nx_gpio_initialize();
-	nx_gpio_set_base_address(0, (void *)PHY_BASEADDR_GPIOA);
-	nx_gpio_set_base_address(1, (void *)PHY_BASEADDR_GPIOB);
-	nx_gpio_set_base_address(2, (void *)PHY_BASEADDR_GPIOC);
-	nx_gpio_set_base_address(3, (void *)PHY_BASEADDR_GPIOD);
-	nx_gpio_set_base_address(4, (void *)PHY_BASEADDR_GPIOE);
-}
-
-#ifdef CONFIG_PWM_NX
-void board_backlight_init(void)
-{
-	nx_gpio_set_pad_function(1, 27, 1);
-	nx_gpio_set_output_enable(1, 27, 1);
-	nx_gpio_set_output_value(1, 27, 1);
-	pwm_init(CONFIG_BACKLIGHT_CH, CONFIG_BACKLIGHT_DIV,
-		 CONFIG_BACKLIGHT_INV);
-	pwm_config(CONFIG_BACKLIGHT_CH, TO_DUTY_NS(CONFIG_BACKLIGHT_DUTY,
-						   CONFIG_BACKLIGHT_HZ),
-		   TO_PERIOD_NS(CONFIG_BACKLIGHT_HZ));
+	dwc2_udc_probe(&nx_otg_data);
 }
 #endif
-
-int mmc_get_env_dev(void)
-{
-	return 0;
-}
 
 int board_init(void)
 {
-	board_gpio_init();
-#ifdef CONFIG_PWM_NX
-	board_backlight_init();
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags |= GD_FLG_SILENT;
+#endif
+
+#if !defined CONFIG_OF_CONTROL && !defined CONFIG_DM_USB &&	\
+	 defined CONFIG_USB_GADGET
+	board_udc_probe();
 #endif
 	return 0;
 }
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+	return 0;
+}
+#endif
 
 /* u-boot dram initialize  */
 int dram_init(void)
@@ -113,84 +70,5 @@ void dram_init_banksize(void)
 
 	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
 	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
-}
-
-#ifdef CONFIG_DM_PMIC_NXE2000
-void pmic_init(void)
-{
-	static struct udevice *dev;
-	int ret = -ENODEV;
-	uint8_t bit_mask = 0;
-
-	ret = pmic_get("nxe2000_gpio@32", &dev);
-	if (ret)
-		printf("Can't get PMIC: %s!\n", "nxe2000_gpio@32");
-
-	bit_mask = pmic_reg_read(dev, NXE2000_REG_PWRONTIMSET);
-	bit_mask &= ~(0x1 << NXE2000_POS_PWRONTIMSET_OFF_JUDGE_PWRON);
-	bit_mask |= (0x0 << NXE2000_POS_PWRONTIMSET_OFF_JUDGE_PWRON);
-	ret = pmic_write(dev, NXE2000_REG_PWRONTIMSET, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC REG: %d!\n", NXE2000_REG_PWRONTIMSET);
-
-	bit_mask = 0x00;
-	ret = pmic_reg_write(dev, (u32)NXE2000_REG_BANKSEL, (u32)bit_mask);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_BANKSEL);
-
-	bit_mask = ((0 << NXE2000_POS_DCxCTL2_DCxOSC) |
-			(0 << NXE2000_POS_DCxCTL2_DCxSR) |
-			(3 << NXE2000_POS_DCxCTL2_DCxLIM) |
-			(0 << NXE2000_POS_DCxCTL2_DCxLIMSDEN));
-	ret = pmic_write(dev, NXE2000_REG_DC1CTL2, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_DC1CTL2);
-
-	bit_mask = ((0 << NXE2000_POS_DCxCTL2_DCxOSC) |
-			(0 << NXE2000_POS_DCxCTL2_DCxSR) |
-			(3 << NXE2000_POS_DCxCTL2_DCxLIM) |
-			(0 << NXE2000_POS_DCxCTL2_DCxLIMSDEN));
-	ret = pmic_write(dev, NXE2000_REG_DC2CTL2, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_DC2CTL2);
-
-	bit_mask = ((0 << NXE2000_POS_DCxCTL2_DCxOSC) |
-			(0 << NXE2000_POS_DCxCTL2_DCxSR) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIM) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIMSDEN));
-	ret = pmic_write(dev, NXE2000_REG_DC3CTL2, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_DC3CTL2);
-
-	bit_mask = ((0 << NXE2000_POS_DCxCTL2_DCxOSC) |
-			(0 << NXE2000_POS_DCxCTL2_DCxSR) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIM) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIMSDEN));
-	ret = pmic_write(dev, NXE2000_REG_DC4CTL2, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_DC4CTL2);
-
-	bit_mask = ((0 << NXE2000_POS_DCxCTL2_DCxOSC) |
-			(0 << NXE2000_POS_DCxCTL2_DCxSR) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIM) |
-			(1 << NXE2000_POS_DCxCTL2_DCxLIMSDEN));
-	ret = pmic_write(dev, NXE2000_REG_DC5CTL2, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_DC5CTL2);
-
-	bit_mask = (1 << NXE2000_POS_CHGCTL1_SUSPEND);
-	ret = pmic_write(dev, NXE2000_REG_CHGCTL1, &bit_mask, 1);
-	if (ret)
-		printf("Can't write PMIC register: %d!\n", NXE2000_REG_CHGCTL1);
-}
-#endif
-
-int board_late_init(void)
-{
-#ifdef CONFIG_DM_PMIC_NXE2000
-	pmic_init();
-#endif
-
-	return 0;
 }
 
