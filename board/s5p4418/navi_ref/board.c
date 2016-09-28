1d3455d6bc0ce9a22cde4deb17e6c996dc4c97b0
jhkim <jhkim@nexell.co.kr>
2016-09-28 16:31:30 +0900

    5p4418 navi_ref: support LVDS display
    This patch support LVDS display for s5p4418 navi ref board.
    
    Change-Id: I1837287be554eb84c5b24d641a82a0ab5a9a7484
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- board/s5p4418/navi_ref/board.c
+++ board/s5p4418/navi_ref/board.c
@@ -29,8 +29,83 @@ static void board_udc_probe(void)
 }
 #endif
 
+#if defined CONFIG_VIDEO &&	\
+	(defined CONFIG_SPL_BUILD ||	\
+	(!defined CONFIG_OF_CONTROL && !defined CONFIG_DM))
+#include <asm/arch/display.h>
+
+#define DP_MODULE		0
+#define DP_DEVICE_TYPE		DP_DEVICE_LVDS
+
+static struct dp_lvds_dev dp_dev = {
+	.lvds_format = 1,	/* 0: VESA, 1: JEIDA */
+};
+
+static struct nx_display_platdata dp_platdata = {
+	.module = DP_MODULE,
+	.sync  = {
+		.pixel_clock_hz = 50000000,
+		.h_active_len = 1024,
+		.v_active_len = 600,
+		.h_front_porch = 160,
+		.h_back_porch = 140,
+		.h_sync_width = 20,
+		.v_front_porch = 12,
+		.v_back_porch = 20,
+		.v_sync_width = 3,
+	},
+
+	.ctrl  =  {
+		.clk_src_lv0 = 3,
+		.clk_div_lv0 = 16,
+		.clk_src_lv1 = 7,
+		.clk_div_lv1 = 1,
+		.out_format = 2,
+	},
+
+	.top = {
+		.screen_width = 1024,
+		.screen_height = 600,
+		.video_prior = 0,
+		.back_color = 0x0,
+	},
+
+	.plane = {
+		[0] = {
+		.fb_base = 0x41000000,
+		.width = 1024,
+		.height = 600,
+		.format = 0x06530000, /* 565: 0x44320000, A888: 0x06530000 */
+		.pixel_byte = 4,
+		},
+	},
+
+	.dev_type = DP_DEVICE_TYPE,
+	.device = &dp_dev,
+};
+#endif
+
+enum gpio_group {
+	gpio_a,	gpio_b, gpio_c, gpio_d, gpio_e,
+};
+
+void board_display_probe(void)
+{
+#if defined CONFIG_SPL_BUILD ||	(	\
+	!defined CONFIG_OF_CONTROL && !defined CONFIG_DM)
+	nx_display_probe(&dp_platdata);
+#endif
+}
+
 int board_init(void)
 {
+#if defined CONFIG_VIDEO
+	/* set pwm0 output off: 1 */
+	nx_gpio_set_pad_function(gpio_d,  1, 0);
+	nx_gpio_set_output_value(gpio_d,  1, 1);
+	nx_gpio_set_output_enable(gpio_d,  1, 1);
+#endif
+
 #ifdef CONFIG_SILENT_CONSOLE
 	gd->flags |= GD_FLG_SILENT;
 #endif
@@ -38,6 +113,9 @@ int board_init(void)
 #if !defined CONFIG_OF_CONTROL && !defined CONFIG_DM_USB &&	\
 	 defined CONFIG_USB_GADGET
 	board_udc_probe();
+#endif
+#if defined CONFIG_VIDEO
+	board_display_probe();
 #endif
 	return 0;
 }
@@ -48,7 +126,38 @@ int board_late_init(void)
 #ifdef CONFIG_SILENT_CONSOLE
 	gd->flags &= ~GD_FLG_SILENT;
 #endif
+
+#if defined CONFIG_VIDEO
+	/* set lcd enable */
+	nx_gpio_set_pad_function(gpio_b, 25, 1);
+	nx_gpio_set_pad_function(gpio_b, 27, 1);
+	nx_gpio_set_pad_function(gpio_c, 11, 1);
+
+	nx_gpio_set_output_value(gpio_b, 25, 1);
+	nx_gpio_set_output_value(gpio_b, 27, 1);
+	nx_gpio_set_output_value(gpio_c, 11, 1);
+
+	nx_gpio_set_output_enable(gpio_b, 25, 1);
+	nx_gpio_set_output_enable(gpio_b, 27, 1);
+	nx_gpio_set_output_enable(gpio_c, 11, 1);
+#endif
+
 	return 0;
+}
+#endif
+
+#ifdef CONFIG_SPL_BUILD
+int spl_display_probe(void)
+{
+#if defined CONFIG_VIDEO
+	board_display_probe();
+#endif
+	return 0;
+}
+
+int spl_late_init(void)
+{
+	return board_late_init();
 }
 #endif
 
