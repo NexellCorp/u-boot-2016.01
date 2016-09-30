3ea669cfaa8a9ea212aba9b68671832b01cadaf0
choonghyun-Jeon <suker@nexell.co.kr>
2016-09-30 18:23:30 +0900

    s5p4418: navi_ref: boot DELAY changed, bootarg add.
    A bootarg append 'consoleblank=0' and Boot DELAY changed to 1 second.
    
    Change-Id: Iec4f1d5e52a5a210dcc6f072edee372583f8f3c1
    Signed-off-by: choonghyun-Jeon <suker@nexell.co.kr>

--- include/configs/s5p4418_navi_ref.h
+++ include/configs/s5p4418_navi_ref.h
@@ -96,7 +96,7 @@
  *	U-Boot Environments
  */
 /* refer to common/env_common.c	*/
-#define CONFIG_BOOTDELAY			0
+#define CONFIG_BOOTDELAY			1
 
 /*-----------------------------------------------------------------------
  * Miscellaneous configurable options
@@ -279,7 +279,7 @@
  */
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 			"fdt_high=0xffffffff\0" \
-                        "bootargs=console=ttyAMA3,115200n8 root=/dev/mmcblk0p3 rw rootfstype=ext4 loglevel=4 rootwait quiet printk.time=1\0" \
+                        "bootargs=console=ttyAMA3,115200n8 root=/dev/mmcblk0p3 rw rootfstype=ext4 loglevel=4 rootwait quiet printk.time=1 consoleblank=0\0" \
                         "boot_cmd_mmcboot="   \
                                    "ext4load mmc 0:1 0x40008000 zImage;ext4load mmc 0:1 49000000 s5p4418-navi_ref-rev00.dtb;" \
                                    "bootz 0x40008000 - 49000000\0" \
