c8f05fe693cbaa7b2d70eda9bd37d2707a5ef785
Hyunseok Jung <hsjung@nexell.co.kr>
2016-11-17 17:37:39 +0900

    armv8/nxp5540/svt: add nxp5540 svt board supports
    This patch enables nxp5540 svt board functions.
    
    Change-Id: I38642b5760329e66e0f3a43fbe40dfd64e95bcbd
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

---
+++ board/nxp5540/svt/board.c
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Hyunseok, Jung <hsjung@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <config.h>
+#include <common.h>
+#include <asm/io.h>
+
+#include <asm/arch/nxp5540.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/*------------------------------------------------------------------------------
+ * intialize nexell soc and board status.
+ */
+
+__weak struct serial_device *default_serial_console(void)
+{
+	return NULL;
+}
+
+/* call from u-boot */
+int board_early_init_f(void)
+{
+	return 0;
+}
+
+int mmc_get_env_dev(void)
+{
+	return 0;
+}
+
+int board_init(void)
+{
+	return 0;
+}
+
+/* u-boot dram initialize  */
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+	return 0;
+}
+
+/* u-boot dram board specific */
+void dram_init_banksize(void)
+{
+	/* set global data memory */
+	gd->bd->bi_arch_number = machine_arch_type;
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x00000100;
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
+
