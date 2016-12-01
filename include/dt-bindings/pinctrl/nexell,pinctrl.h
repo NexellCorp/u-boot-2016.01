91e07e9c3384a7e125099006b8f9628c45e68305
Bon-gyu, KOO <freestyle@nexell.co.kr>
2016-12-01 14:42:28 +0900

    dtsi: add pinctrl PAD pull macro
    Previous the numbering scheme was so confusing,
    changed to use macros.
    
    Change-Id: I63a0e4700088628c671d5fed50a01c545e5b2841
    Signed-off-by: Bon-gyu, KOO <freestyle@nexell.co.kr>

---
+++ include/dt-bindings/pinctrl/nexell,pinctrl.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Bon-gyu, KOO <freestyle@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef _NEXELL_PINCTRL_H
+#define _NEXELL_PINCTRL_H
+
+#define PAD_PULL_UP		0
+#define PAD_PULL_DOWN		1
+#define PAD_PULL_OFF		2
+
+#endif /* _NEXELL_PINCTRL_H */
