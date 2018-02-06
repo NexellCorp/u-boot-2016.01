d2dbfcd7d57a18ab8011b2cb0540daf9612ad044
Sungwoo Park <swpark@nexell.co.kr>
2018-02-06 15:03:32 +0900

    quickboot/navi_ref: Apply quickboot patch
    This patch applies QUICKBOOT patch.
    
    Following is primary changes.
    - removes CONFIG_SYS_DCACHE_OFF
    - removes CONFIG_CMD_MEMTEST
    - adds CONFIG_SYS_CONSOLE_INFO_QUIET
    - adds CONFIG_BOOST_MMC
    - implements board_set_mmc_pre() function
    
    Change-Id: I7bb66badf14cfde17b00a0acb6f1aac2ebc7ea92
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- board/s5p4418/navi_ref/board.c
+++ board/s5p4418/navi_ref/board.c
@@ -109,7 +109,9 @@ int board_late_init(void)
 #define ALIVE_SCRATCH1_READ_REGISTER	(0xc00108b4)
 #define ALIVE_SCRATCH1_RESET_REGISTER	(0xc00108ac)
 #define RECOVERY_SIGNATURE				(0x52455343)    /* (ASCII) : R.E.S.C */
+#ifndef QUICKBOOT
 	printf("signature --> 0x%x\n", readl(ALIVE_SCRATCH1_READ_REGISTER));
+#endif
 	if (readl(ALIVE_SCRATCH1_READ_REGISTER) == RECOVERY_SIGNATURE) {
 		printf("reboot recovery!!!!\n");
 		writel(0xffffffff, ALIVE_SCRATCH1_RESET_REGISTER);
@@ -170,3 +172,49 @@ void dram_init_banksize(void)
 	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
 	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
 }
+
+#ifdef QUICKBOOT
+#include <mmc.h>
+
+int board_set_mmc_pre(struct mmc *mmc)
+{
+	mmc->version = 1074069504;
+	mmc->high_capacity = 1;
+	mmc->bus_width = 4;
+	mmc->clock = 50000000;
+	mmc->card_caps = 0x7;
+	mmc->ocr = 0xc0ff8080;
+	mmc->dsr = 0xffffffff;
+	mmc->dsr_imp = 0x0;
+	mmc->scr[0] = 0x0;
+	mmc->scr[1] = 0x0;
+	mmc->csd[0] = 0xd0270132;
+	mmc->csd[1] = 0x0f5903ff;
+	mmc->csd[2] = 0xf6dbffef;
+	mmc->csd[3] = 0x8e40400d;
+	mmc->cid[0] = 0x15010038;
+	mmc->cid[1] = 0x474e4433;
+	mmc->cid[2] = 0x5201aaf9;
+	mmc->cid[3] = 0x5051245b;
+	mmc->rca = 0x1;
+	mmc->part_support = 7;
+	mmc->part_num = 0;
+	mmc->tran_speed = 52000000;
+	mmc->read_bl_len = 512;
+	mmc->write_bl_len = 512;
+	mmc->erase_grp_size = 1024;
+	mmc->hc_wp_grp_size = 16384;
+	mmc->capacity = 7818182656;
+	mmc->capacity_user = 7818182656;
+	mmc->capacity_rpmb = 524288;
+	mmc->capacity_gp[0] = 0;
+	mmc->capacity_gp[1] = 0;
+	mmc->capacity_gp[2] = 0;
+	mmc->capacity_gp[3] = 0;
+	mmc->enh_user_start = 8;
+	mmc->enh_user_size = 8;
+	mmc->ddr_mode = 0;
+
+	return 1;
+}
+#endif
