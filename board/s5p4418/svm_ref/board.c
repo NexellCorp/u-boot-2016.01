ad599aff01a4ac3d880b2179ba6eb9fe3c9e4143
Min GyoungBo <mingyoungbo@nexell.co.kr>
2018-03-05 11:39:07 +0900

    s5p4418: svm: Introduce svm board
    This patch introduces svm board based on s5p4418 platform.
    
    Change-Id: Id46ddbae4b89af40e0ea441240cc3e0ef8356890
    Signed-off-by: Min GyoungBo <mingyoungbo@nexell.co.kr>

---
+++ board/s5p4418/svm_ref/board.c
@@ -0,0 +1,190 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * gyoungbo, min <mingyoungbo@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <config.h>
+#include <common.h>
+#include <asm/io.h>
+
+#include <asm/arch/nexell.h>
+#include <asm/arch/nx_gpio.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#ifdef CONFIG_PWM_NX
+enum gpio_group {
+	gpio_a,	gpio_b, gpio_c, gpio_d, gpio_e,
+};
+
+struct pwm_device {
+	int grp;
+	int bit;
+	int io_fn;
+};
+
+static struct pwm_device pwm_dev[] = {
+	[0] = { .grp = gpio_d, .bit = 1,  .io_fn = 0 },
+	[1] = { .grp = gpio_c, .bit = 13, .io_fn = 1 },
+	[2] = { .grp = gpio_c, .bit = 14, .io_fn = 1 },
+	[3] = { .grp = gpio_d, .bit = 0,  .io_fn = 0 },
+};
+#endif
+
+static void board_backlight_disable(void)
+{
+#ifdef CONFIG_PWM_NX
+	int gp = pwm_dev[CONFIG_BACKLIGHT_CH].grp;
+	int io = pwm_dev[CONFIG_BACKLIGHT_CH].bit;
+	int fn = pwm_dev[CONFIG_BACKLIGHT_CH].io_fn;
+
+	/*
+	 * pwm backlight OFF: HIGH, ON: LOW
+	 */
+	nx_gpio_set_pad_function(gp, io, fn);
+	nx_gpio_set_output_value(gp, io, 1);
+	nx_gpio_set_output_enable(gp, io, 1);
+
+	/*
+	 * set lcd enable
+	 */
+#if 0
+	nx_gpio_set_pad_function(gpio_c, 11, 1);
+	nx_gpio_set_pad_function(gpio_b, 25, 1);
+	nx_gpio_set_pad_function(gpio_b, 27, 1);
+
+	nx_gpio_set_output_value(gpio_c, 11, 1);
+	nx_gpio_set_output_value(gpio_b, 25, 1);
+	nx_gpio_set_output_value(gpio_b, 27, 1);
+
+	nx_gpio_set_output_enable(gpio_c, 11, 1);
+	nx_gpio_set_output_enable(gpio_b, 25, 1);
+	nx_gpio_set_output_enable(gpio_b, 27, 1);
+#else
+	nx_gpio_set_pad_function(gpio_e, 1, 0);
+	nx_gpio_set_pad_function(gpio_b, 25, 1);
+	nx_gpio_set_pad_function(gpio_e, 13, 0);
+	nx_gpio_set_pad_function(gpio_b, 27, 1);
+
+	nx_gpio_set_output_value(gpio_e, 1, 1);
+	nx_gpio_set_output_value(gpio_b, 25, 1);
+	nx_gpio_set_output_value(gpio_e, 13, 0);
+	nx_gpio_set_output_value(gpio_b, 27, 1);
+
+	nx_gpio_set_output_enable(gpio_e, 1, 1);
+	nx_gpio_set_output_enable(gpio_b, 25, 1);
+	nx_gpio_set_output_enable(gpio_e, 13, 1);
+	mdelay(130);
+	nx_gpio_set_output_enable(gpio_b, 27, 1);
+#endif
+#endif
+}
+
+static void board_backlight_enable(void)
+{
+#ifdef CONFIG_PWM_NX
+	/*
+	 * pwm backlight ON: HIGH, ON: LOW
+	 */
+	pwm_init(
+		CONFIG_BACKLIGHT_CH,
+		CONFIG_BACKLIGHT_DIV, CONFIG_BACKLIGHT_INV
+		);
+	pwm_config(
+		CONFIG_BACKLIGHT_CH,
+		TO_DUTY_NS(CONFIG_BACKLIGHT_DUTY, CONFIG_BACKLIGHT_HZ),
+		TO_PERIOD_NS(CONFIG_BACKLIGHT_HZ)
+		);
+#endif
+}
+
+int board_init(void)
+{
+	board_backlight_disable();
+
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags |= GD_FLG_SILENT;
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+	board_backlight_enable();
+
+#ifdef CONFIG_RECOVERY_BOOT
+#define ALIVE_SCRATCH1_READ_REGISTER	(0xc00108b4)
+#define ALIVE_SCRATCH1_RESET_REGISTER	(0xc00108ac)
+#define RECOVERY_SIGNATURE				(0x52455343)    /* (ASCII) : R.E.S.C */
+	printf("signature --> 0x%x\n", readl(ALIVE_SCRATCH1_READ_REGISTER));
+	if (readl(ALIVE_SCRATCH1_READ_REGISTER) == RECOVERY_SIGNATURE) {
+		printf("reboot recovery!!!!\n");
+		writel(0xffffffff, ALIVE_SCRATCH1_RESET_REGISTER);
+		setenv("bootcmd", "run recoveryboot");
+	}
+#endif
+
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_SPLASH_SOURCE
+#include <splash.h>
+struct splash_location splash_locations[] = {
+	{
+	.name = "mmc_fs",
+	.storage = SPLASH_STORAGE_MMC,
+	.flags = SPLASH_STORAGE_FS,
+	.devpart = "0:1",
+	},
+	{
+	.name = "mmc",
+	.storage = SPLASH_STORAGE_MMC,
+	.flags = SPLASH_STORAGE_RAW,
+	.offset = CONFIG_SPLASH_MMC_OFFSET,
+	},
+};
+
+int splash_screen_prepare(void)
+{
+	int err = splash_source_load(splash_locations,
+					sizeof(splash_locations)/sizeof(struct splash_location));
+	if (!err) {
+		char addr[64];
+
+		sprintf(addr, "0x%lx", gd->fb_base);
+		setenv("fb_addr", addr);
+	}
+
+	return err;
+}
+#endif
+
+/* u-boot dram initialize  */
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+	return 0;
+}
+
+/* u-boot dram board specific */
+void dram_init_banksize(void)
+{
+	/* set global data memory */
+	gd->bd->bi_arch_number = machine_arch_type;
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x00000100;
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
+}
