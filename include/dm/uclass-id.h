b0596eae7ad5a8d0ca910c7a36e5a1bc354da32b
HyejungKwon <cjscld15@nexell.co.kr>
2018-03-27 16:54:25 +0900

    misc: nx_i2c: remove nx_i2c driver
    This patch removes a nx_i2c driver
    
    Change-Id: I78775c162f309d00549b4a16ce9004f4164024b9
    Signed-off-by: HyejungKwon <cjscld15@nexell.co.kr>

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -73,7 +73,6 @@ enum uclass_id {
 	UCLASS_SENSOR_ID,	/* Sensor type detecting device */
 	UCLASS_USBCHARGE_ID,	/* USB Charger */
 	UCLASS_UB927928_ID,     /* Serdes */
-	UCLASS_NX_I2C_ID,
 	UCLASS_NX_I2C_GPIO,
 	UCLASS_COUNT,
 	UCLASS_INVALID = -1,
