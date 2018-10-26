808853349964f72004027a1b6a3fdb57580b8639
Jinyong Lee <justin@nexell.co.kr>
2018-10-26 15:48:31 +0900

    s5p6818: bitminer: add adc test for MFG
    Change-Id: I860ac44599d2de90190504c4dc66bae31c265678
    Signed-off-by: Jinyong Lee <justin@nexell.co.kr>

--- board/s5p6818/bitminer_ref/cmd_mfgtest.c
+++ board/s5p6818/bitminer_ref/cmd_mfgtest.c
@@ -65,6 +65,33 @@ static void ok_led_no_return(void)
 		udelay(300000);
 	}
 }
+
+/* 0x000 : 0V
+ * 0xFFF : 1.8V
+ * (1.8/4096)xADC = voltage
+ */
+#define ad2mV(adc)	((adc*1800)/4096)
+
+#define HASH_ADC_MIN	0//1550
+#define HASH_ADC_MAX	1800//1750
+
+static int test_adc(int ch, int *val)
+{
+	int ret = 0;
+	int volt;
+	writel((6<<6)+((ch&0x7)<<3)+0, PHY_BASEADDR_ADC+0x0);
+	writel((6<<6)+((ch&0x7)<<3)+1, PHY_BASEADDR_ADC+0x0);
+	udelay(10000);
+	ret = readl(PHY_BASEADDR_ADC+0x4);
+	ret &= 0xfff;
+	volt = ad2mV(ret);
+	printf("ADC CH%d : %d.%03dV (0x%03X)\n", ch, volt/1000, volt%1000, ret);
+	*val = ret;
+	if(volt<=HASH_ADC_MIN) return -1;
+	if(volt>=HASH_ADC_MAX) return -1;
+	return 0;
+}
+
 // AP_SPI0_CLK		: GPIOC29
 // AP_SPI0_CS 		: GPIOC30
 // AP_SPI0_TX 		: GPIOC31
@@ -88,7 +115,15 @@ static void ok_led_no_return(void)
 // AP_HASH1_RST		: GPIOE4
 static int test_hash_con(void)
 {
-	int ret = 0;
+	int ret = 0, adc_val;
+
+	// init ADC
+	nx_rstcon_setrst(RESET_ID_ADC, 1);
+	writel(166, PHY_BASEADDR_ADC+0x10);
+	writel((1<<15)+166, PHY_BASEADDR_ADC+0x10);
+	writel((6<<6), PHY_BASEADDR_ADC+0x0);
+	writel((6<<6)+1, PHY_BASEADDR_ADC+0x0);
+
 	// set HASH 0 pins to out
 	nx_gpio_set_pad_function  (gpio_c, 29, 0);	// AP_SPI0_CLK
 	nx_gpio_set_pull_mode     (gpio_c, 29, 2);
@@ -151,27 +186,27 @@ static int test_hash_con(void)
 	nx_gpio_set_pad_function  (gpio_a,  9, 0);	// AP_HASH1_VOLCTRL
 	nx_gpio_set_pull_mode     (gpio_a,  9, 2);
 	nx_gpio_set_drive_strength(gpio_a,  9, 3);
-	nx_gpio_set_output_enable (gpio_a,  9, 1);
+	nx_gpio_set_output_enable (gpio_a,  9, 0);
 	nx_gpio_set_pad_function  (gpio_a, 11, 0);	// AP_HASH1_PLUG
 	nx_gpio_set_pull_mode     (gpio_a, 11, 2);
 	nx_gpio_set_drive_strength(gpio_a, 11, 3);
-	nx_gpio_set_output_enable (gpio_a, 11, 1);
+	nx_gpio_set_output_enable (gpio_a, 11, 0);
 	nx_gpio_set_pad_function  (gpio_a, 16, 0);	// AP_HASH1_PWREN
 	nx_gpio_set_pull_mode     (gpio_a, 16, 2);
 	nx_gpio_set_drive_strength(gpio_a, 16, 3);
-	nx_gpio_set_output_enable (gpio_a, 16, 1);
+	nx_gpio_set_output_enable (gpio_a, 16, 0);
 	nx_gpio_set_pad_function  (gpio_e,  2, 0);	// AP_HASH1_OON
 	nx_gpio_set_pull_mode     (gpio_e,  2, 2);
 	nx_gpio_set_drive_strength(gpio_e,  2, 3);
-	nx_gpio_set_output_enable (gpio_e,  2, 1);
+	nx_gpio_set_output_enable (gpio_e,  2, 0);
 	nx_gpio_set_pad_function  (gpio_e,  3, 0);	// AP_HASH1_GLD
 	nx_gpio_set_pull_mode     (gpio_e,  3, 2);
 	nx_gpio_set_drive_strength(gpio_e,  3, 3);
-	nx_gpio_set_output_enable (gpio_e,  3, 1);
+	nx_gpio_set_output_enable (gpio_e,  3, 0);
 	nx_gpio_set_pad_function  (gpio_e,  4, 0);	// AP_HASH1_RST
 	nx_gpio_set_pull_mode     (gpio_e,  4, 2);
 	nx_gpio_set_drive_strength(gpio_e,  4, 3);
-	nx_gpio_set_output_enable (gpio_e,  4, 1);
+	nx_gpio_set_output_enable (gpio_e,  4, 0);
 
 	nx_gpio_set_output_value  (gpio_c, 29, 0);	// AP_SPI0_CLK
 	nx_gpio_set_output_value  (gpio_c, 30, 0);	// AP_SPI0_CS
@@ -183,6 +218,8 @@ static int test_hash_con(void)
 	nx_gpio_set_output_value  (gpio_d, 29, 0);	// AP_HASH0_OON
 	nx_gpio_set_output_value  (gpio_d, 30, 0);	// AP_HASH0_GLD
 	nx_gpio_set_output_value  (gpio_d, 31, 0);	// AP_HASH0_RST
+
+	udelay(10000);
 
 	if(
 		nx_gpio_get_input_value  (gpio_c,  9)	// AP_SPI2_CLK
@@ -256,6 +293,8 @@ static int test_hash_con(void)
 	nx_gpio_set_output_value  (gpio_d, 30, 1);	// AP_HASH0_GLD
 	nx_gpio_set_output_value  (gpio_d, 31, 1);	// AP_HASH0_RST
 
+	udelay(10000);
+
 	if(
 		nx_gpio_get_input_value  (gpio_c,  9)	// AP_SPI2_CLK
 	  != 1) {
@@ -317,27 +356,30 @@ static int test_hash_con(void)
 		ret++;
 	}
 
-	/* TODO: run AP_HASH0_ADC(ADC0) for detecting V3P3_HASH1 */
+	if(test_adc(0, &adc_val)) {
+		printf("AP_HASH0_ADC are not connected (0x%03x)\n", adc_val);
+		ret++;
+	}
 
-	/* TODO: run AP_HASH1_ADC(ADC1) for detecting V3P3_HASH0 */
+	if(test_adc(1, &adc_val)) {
+		printf("AP_HASH1_ADC are not connected (0x%03x)\n", adc_val);
+		ret++;
+	}
 
 	return ret;
 }
 
 static int do_mfgtest(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-//	test_led_key();
+	test_led_key();
 
 	set_gled_off();
 	set_rled_on();
-//	if(test_hash_con()) error_led_no_return();
+	if(test_hash_con()) error_led_no_return();
 
 	set_gled_on();
 	set_rled_off();
 	return 0;
-
-bat_cmd_usage:
-	return cmd_usage(cmdtp);
 }
 
 U_BOOT_CMD(
@@ -351,9 +393,6 @@ static int do_okled(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	ok_led_no_return();
 
 	return 0;
-
-bat_cmd_usage:
-	return cmd_usage(cmdtp);
 }
 
 U_BOOT_CMD(
@@ -424,7 +463,7 @@ static int do_mfgmac(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	if(setenv("ethaddr", vars)) error_led_no_return();
 	if(saveenv()) error_led_no_return();
 
-	if(run_command("fdisk 0 3: 0x400000:0x4000000 0x4400000:0x2000000 0x6400000:0x9000000", 0)) error_led_no_return();
+	if(run_command("fdisk 0 5: 0x400000:0x4000000 0x4400000:0x2000000 0x6400000:0x9000000 0xf500000:0x10000000 0x1f500000:0", 0)) error_led_no_return();
 	if(run_command("tftpboot 40000000 boot.img; ext4_img_write 0 0000000040000000 1 $filesize", 0)) error_led_no_return();
 //	if(run_command("tftpboot 40000000 modules.img; mmc write 40000000 22000 10000", 0)) error_led_no_return();
 	if(run_command("tftpboot 40000000 rootfs.img; ext4_img_write 0 0000000040000000 3 $filesize", 0)) error_led_no_return();
@@ -432,9 +471,6 @@ static int do_mfgmac(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	ok_led_no_return();
 
 	return 0;
-
-bat_cmd_usage:
-	return cmd_usage(cmdtp);
 }
 
 U_BOOT_CMD(
@@ -442,6 +478,7 @@ U_BOOT_CMD(
 	"get mac from tftp server and write it to env area\n",
 	""
 );
+
 static int do_mfgflash(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	char vars[256];
@@ -458,14 +495,67 @@ static int do_mfgflash(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 	if(run_command("reset", 0)) error_led_no_return();
 
 	return 0;
-
-bat_cmd_usage:
-	return cmd_usage(cmdtp);
 }
 
 U_BOOT_CMD(
 	mfgflash, 2, 1, do_mfgflash,
 	"burn files to mmc for mmc booting\n",
+	""
+);
+
+static int do_mfg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	int port_num = -1;
+	int boot_mode = readl(PHY_BASEADDR_CLKPWR + SYSRSTCONFIG);
+
+	if ((boot_mode & BOOTMODE_MASK) == BOOTMODE_SDMMC)
+		port_num = readl(SCR_ARM_SECOND_BOOT_REG1);
+
+	// if USB boot
+	if(port_num == -1)
+		return -1;
+
+	// if SDCARD boot
+	if(port_num == SD_PORT_NUM) {
+		run_command("mfgflash", 0);
+	}
+	// if MMC boot
+	if(port_num == EMMC_PORT_NUM) {
+		run_command("mfgmac", 0);
+	}
+	return 0;
+}
+
+U_BOOT_CMD(
+	mfg, 2, 1, do_mfg,
+	"test and fusing, write mac\n",
+	""
+);
+
+static int do_bootdev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	int ret = 0;
+	int port_num;
+	int boot_mode = readl(PHY_BASEADDR_CLKPWR + SYSRSTCONFIG);
+
+	if ((boot_mode & BOOTMODE_MASK) == BOOTMODE_SDMMC) {
+		port_num = readl(SCR_ARM_SECOND_BOOT_REG1);
+		printf("boot: SDMMC(0x%08x) (port = %d)\n", boot_mode, port_num);
+
+//		if (port_num == EMMC_PORT_NUM)
+//		else if (port_num == SD_PORT_NUM)
+	} else if ((boot_mode & BOOTMODE_MASK) == BOOTMODE_USB) {
+		printf("boot: USB(0x%08x)\n", boot_mode);
+	}
+	else
+		printf("boot: unknown(0x%08x)\n", boot_mode);
+
+	return ret;
+}
+
+U_BOOT_CMD(
+	bootdev, 2, 1, do_bootdev,
+	"print boot media\n",
 	""
 );
 #endif /* CONFIG_CMD_MFGTEST */
