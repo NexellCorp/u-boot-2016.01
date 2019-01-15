058731777aa15cd2707827ab2193c1b0ddc3ab83
Youngbok Park <ybpark@nexell.co.kr>
2019-01-15 17:10:41 +0900

    nxp4330: add board vtk
    This patch add nxp4330 vtk board.
    
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>
    Change-Id: I8a4d72c662bdf0d262d50d44e8003b8827255191

---
+++ board/nexell/s5p4418/vtk/board.c
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Youngbok, Park <ybpark@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <config.h>
+#include <common.h>
+#include <asm/io.h>
+
+#include <asm/arch/nexell.h>
+#include <asm/arch/nx_gpio.h>
+
+#include <asm-generic/gpio.h>
+#include <nx_i2c_gpio.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags |= GD_FLG_SILENT;
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+
+
+	return 0;
+}
+#endif
+
+/* u-boot dram initialize  */
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+	return 0;
+}
+
+/* u-boot dram board specific */
+void dram_init_banksize(void)
+{
+	/* set global data memory */
+	gd->bd->bi_arch_number = machine_arch_type;
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x00000100;
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
+}
+
