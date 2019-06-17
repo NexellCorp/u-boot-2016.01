613945c3973fab3000c864518ecee3927cf31238
seoji <seoji@nexell.co.kr>
2019-06-17 18:40:15 +0900

    Add adc common functions for board revision check feature
    This patch adds function related getting adc data and sorting adc data.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>
    Change-Id: Id7be6899b05373b5e1e7cc198b7b28e3f3d6856f

---
+++ arch/arm/mach-nexell/nx_adc.c
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Seoji, Kim <seoji@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <common.h>
+#include <linux/types.h>
+#include <asm/io.h>
+#include <adc.h>
+
+#define MAX_ADC_SIZE 15
+static void quick_sort(unsigned int *data, int start, int end)
+{
+	int pivot = start;
+	int i = pivot + 1;
+	int j = end;
+	int temp;
+
+	if (start >= end) {
+		return;
+	}
+
+	while (i <= j) {
+		while (i <= end && data[i] <= data[pivot]) {
+			i++;
+		}
+		while (j > start && data[j] >= data[pivot]) {
+			j--;
+		}
+
+		if (i > j) {
+			temp = data[j];
+			data[j] = data[pivot];
+			data[pivot] = temp;
+		} else {
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+	}
+
+	quick_sort(data, start, j - 1);
+	quick_sort(data, j + 1, end);
+}
+
+static int get_nexell_adc_val(int channel)
+{
+	volatile int i;
+	int start = 5;
+	int end = MAX_ADC_SIZE -1;
+	unsigned int adcval[MAX_ADC_SIZE];
+
+	for (i = 0; i < MAX_ADC_SIZE; i++) {
+		udelay(1);
+		if (adc_channel_single_shot("adc", channel, &adcval[i]))
+			*(adcval+i) = 0;
+	}
+
+	/* quick sort */
+	quick_sort(adcval, start, end);
+
+	return adcval[(start + end) / 2];
+}
