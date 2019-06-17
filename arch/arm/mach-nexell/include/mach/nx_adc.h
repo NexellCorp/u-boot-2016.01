07f9d63ce552016f99401d016ab2f567ff9e530c
seoji <seoji@nexell.co.kr>
2019-06-17 18:40:15 +0900

    Add adc common functions for board revision check feature
    This patch adds function related getting adc data and sorting adc data.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>
    Change-Id: Id7be6899b05373b5e1e7cc198b7b28e3f3d6856f
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2016.01/+/458251
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ arch/arm/mach-nexell/include/mach/nx_adc.h
@@ -0,0 +1,13 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Seoji, Kim <seoji@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __NX_ADC_H__
+#define __NX_ADC_H__
+
+int get_nexell_adc_val(int channel);
+
+#endif
