1906836a994612f39ef1e069350b92bdd2401da2
seoji <seoji@nexell.co.kr>
2019-06-19 16:37:24 +0900

    board: nexell: s5p4418_con_svma: Add clipper and decimator node selection feature
    This patch adds feature selecting clipper and decimator node according
    to camera input selection.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>
    Change-Id: Iece72648583d7dd1c388c6d3d5ac7dd6bf9e9dbb

--- include/configs/s5p4418_con_svma.h
+++ include/configs/s5p4418_con_svma.h
@@ -389,6 +389,20 @@
                 "\0" \
         "set_bootargs_ab1=setenv bootargs \"${bootargs} ${bootargs_ab1}\" \0" \
         "set_bootargs_ab2=setenv bootargs \"${bootargs} ${bootargs_ab2}\" \0" \
+	"change_devicetree=run set_camera_input\0" \
+	"set_camera_input=" \
+	"fdt addr 406e6800;"	\
+	"if test ${cam_input} -eq 0; then " \
+		"fdt set /soc/clipper1 status okay;" \
+		"fdt set /soc/decimator_1 status okay;" \
+		"fdt set /clipper6 status disabled;" \
+		"fdt set /decimator6 status disabled;" \
+	"elif test ${cam_input} -eq 1; then " \
+		"fdt set /soc/clipper1 status disabled;" \
+		"fdt set /soc/decimator_1 status disabled;" \
+		"fdt set /clipper6 status okay;" \
+		"fdt set /decimator6 status okay;" \
+	"fi;\0" \
         "bootcmd_set_ab=run set_ab_select;" \
                        "run set_bootargs_ab1;" \
                        "run set_bootargs_ab2;" \
