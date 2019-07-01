adb411f554de1c3ef4a92f068f71056b4bda8a74
seoji <seoji@nexell.co.kr>
2019-07-01 18:09:30 +0900

    Fix adc common function for board and camera input check feature
    This patch modify getting reading adc data function.
    
    Signed-off-by: seoji <seoji@nexell.co.kr>
    Change-Id: I2b1e984acf9f1d70450b7baa1ce6f7df694814b6

--- arch/arm/mach-nexell/nx_adc.c
+++ arch/arm/mach-nexell/nx_adc.c
@@ -10,8 +10,7 @@
 #include <asm/io.h>
 #include <adc.h>
 
-#define MAX_ADC_SIZE 15
-#define MAX_ADC_SAMPELING 100
+#define MAX_ADC_SAMPELING 5
 #define ADC_THRESHOLD 2047
 
 int get_nexell_adc_val(int channel)
@@ -19,20 +18,20 @@ int get_nexell_adc_val(int channel)
 	volatile int i;
 	unsigned int adcval;
 
+	/* pclk: 200Mhz prescaler: 166 -> 1 clk (0.8us) */
+	/* To read valid data, mimimum 6 clocks are needed */
+	udelay(5);
+
 	for(i = 0; i < MAX_ADC_SAMPELING; i++) {
-		udelay(1);
+
 		if (adc_channel_single_shot("adc", channel, &adcval))
 			adcval = 0;
-		if (i >= 4) {
-			/* high */
-			if (adcval > ADC_THRESHOLD) {
-				break;
-			/* low */
-			} else {
-				if (i >= MAX_ADC_SIZE)
-					break;
-			}
-		}
+
+		if ((i > 0) && (adcval > ADC_THRESHOLD))
+			return adcval;
+
+		/* 5 cycles are needed per conversion */
+		udelay(4);
 	}
 	return adcval;
 }
