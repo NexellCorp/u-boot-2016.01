3e64c8cb89b9969735c1534d9ec250af0046d195
GyoungBo Min <mingyoungbo@nexell.co.kr>
2019-07-31 12:30:49 +0900

    board: nexell: s5p6818_con_svma: Add Micom(STM32) Fusing Command
    This patch enable Micom(STM32) Fusing command.
    example>
    u-boot$ udown 48000000
    
    host linux$ cd {RESULT}
    host linux$ sudo ./usb-downloader -t slsiap -f micom.bin
    
    u-boot$ micom 49000000
    
    Change-Id: Ic48c5da2ac2c61865a0f03882dd15c539f969758
    Signed-off-by: GyoungBo Min <mingyoungbo@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2016.01/+/463714
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>

--- include/configs/s5p6818_con_svma.h
+++ include/configs/s5p6818_con_svma.h
@@ -143,10 +143,18 @@
 /*-----------------------------------------------------------------------
  * serial console configuration
  */
+#define CONFIG_MCU_DOWNLOAD		1 /* set serial (download to mcu) */
+#define CONFIG_S5P_SERIAL_MCU_INDEX	4
+#define CONFIG_BAUDRATE_MCU		57600
+
 #define CONFIG_S5P_SERIAL
 #define CONFIG_S5P_SERIAL_INDEX     0
 #define CONFIG_S5P_SERIAL_CLOCK     50000000
-#define CONFIG_S5P_SERIAL_PORT      (void *)PHY_BASEADDR_UART0
+#define CONFIG_S5P_SERIAL_PORT		{(void *)PHY_BASEADDR_UART0, \
+					(void *)PHY_BASEADDR_UART1, \
+					(void *)PHY_BASEADDR_UART2, \
+					(void *)PHY_BASEADDR_UART3, \
+					(void *)PHY_BASEADDR_UART4}
 
 #define CONFIG_BAUDRATE				115200
 #define CONFIG_SYS_BAUDRATE_TABLE \
