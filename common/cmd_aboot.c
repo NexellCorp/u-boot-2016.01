e6728077001ef23b7818c0bff7722e37ee81c1a4
allan park <allan.park@nexell.co.kr>
2019-08-28 16:31:56 +0900

    common: cmd_aboot: load_zImage, load_kernel, load_ramdisk function
    This patch add load_zImage, kernel ramdisk functions.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: I6927cf7bb60336b2f201f8eeda0d5ea8def310c1

--- common/cmd_aboot.c
+++ common/cmd_aboot.c
@@ -6,6 +6,7 @@
 
 #include <common.h>
 #include <mapmem.h>
+#include <mmc.h>
 #include <android_image.h>
 
 static int do_aboot_dump(cmd_tbl_t *cmdtp, int flag, int argc,
@@ -73,7 +74,7 @@ static int do_aboot_load_mmc(cmd_tbl_t *cmdtp, int flag, int argc,
 	kernel_size = hdr->kernel_size;
 	ramdisk_size = hdr->ramdisk_size;
 	page_size = hdr->page_size;
-	mmc_blksz = 512;
+	mmc_blksz = MMC_MAX_BLOCK_LEN;
 
 	/* hdr size : 1 page */
 	kernel_offset = bootimg_blk + (ALIGN(sizeof(struct andr_img_hdr), page_size) >> 9);
@@ -102,12 +103,147 @@ static int do_aboot_load_mmc(cmd_tbl_t *cmdtp, int flag, int argc,
 
 	return CMD_RET_SUCCESS;
 }
+static int do_aboot_load_zImage(cmd_tbl_t *cmdtp, int flag, int argc,
+			  char * const argv[])
+{
+	char command[256] = {0, };
+	const struct andr_img_hdr *hdr;
+	u32 bootimg_blk;
+	u32 count;
+	u32 kernel_addr;
+	u32 kernel_size;
+	u32 kernel_offset;
+	u32 page_size, mmc_blksz;
+
+	bootimg_blk = simple_strtoul(argv[1], 0, 16);
+	kernel_addr = simple_strtoul(argv[2], 0, 16);
+
+	/* Step 1 : read bootimage_header(512byte) */
+	count = 1;
+	sprintf(command, "mmc read 0x%x 0x%x 0x%x", kernel_addr, bootimg_blk, count);
+	run_command(command, 0);
+
+	hdr = map_sysmem(kernel_addr, sizeof(*hdr));
+
+	kernel_size = hdr->kernel_size;
+	page_size = hdr->page_size;
+	mmc_blksz = MMC_MAX_BLOCK_LEN;
+
+	/* hdr size : 1 page */
+	kernel_offset = bootimg_blk + (ALIGN(sizeof(struct andr_img_hdr), page_size) >> 9);
+
+	unmap_sysmem(hdr);
+
+	/* Step 2 : load kernel to address  */
+	count = ALIGN(kernel_size, mmc_blksz) >> 9;
+
+	sprintf(command, "mmc read 0x%x 0x%x 0x%x",
+			kernel_addr, kernel_offset, count);
+	run_command(command, 0);
+
+	return CMD_RET_SUCCESS;
+}
+
+
+static int do_aboot_load_kernel(cmd_tbl_t *cmdtp, int flag, int argc,
+			  char * const argv[])
+{
+	char command[256] = {0, };
+	const struct andr_img_hdr *hdr;
+	u32 bootimg_blk;
+	u32 count;
+	u32 kernel_addr;
+	u32 kernel_size;
+	u32 kernel_offset;
+	u32 page_size, mmc_blksz;
+
+	bootimg_blk = simple_strtoul(argv[1], 0, 16);
+	kernel_addr = simple_strtoul(argv[2], 0, 16);
+
+	/* Step 1 : read bootimage_header(512byte) */
+	count = 1;
+	sprintf(command, "mmc read 0x%x 0x%x 0x%x", kernel_addr, bootimg_blk, count);
+	run_command(command, 0);
+
+	hdr = map_sysmem(kernel_addr, sizeof(*hdr));
+
+	kernel_size = hdr->kernel_size;
+	page_size = hdr->page_size;
+	mmc_blksz = MMC_MAX_BLOCK_LEN;
+
+	kernel_offset = bootimg_blk;
+
+	unmap_sysmem(hdr);
+
+	/* Step 2 : load kernel to address  */
+	count =  (ALIGN(sizeof(struct andr_img_hdr), page_size) >> 9) +
+			(ALIGN(kernel_size, mmc_blksz) >> 9 );
+
+	sprintf(command, "mmc read 0x%x 0x%x 0x%x",
+			kernel_addr, kernel_offset, count);
+	run_command(command, 0);
+
+	return CMD_RET_SUCCESS;
+}
+
+static int do_aboot_load_ramdisk(cmd_tbl_t *cmdtp, int flag, int argc,
+			  char * const argv[])
+{
+	char command[256] = {0, };
+	const struct andr_img_hdr *hdr;
+	u32 bootimg_blk;
+	u32 count;
+	u32 ramdisk_addr;
+	u32 kernel_size, ramdisk_size;
+	u32 kernel_offset, ramdisk_offset;
+	u32 page_size, mmc_blksz;
+
+	bootimg_blk = simple_strtoul(argv[1], 0, 16);
+	ramdisk_addr = simple_strtoul(argv[2], 0, 16);
+
+	/* Step 1 : read bootimage_header(512byte) */
+	count = 1;
+	sprintf(command, "mmc read 0x%x 0x%x 0x%x", ramdisk_addr, bootimg_blk, count);
+	run_command(command, 0);
+
+	hdr = map_sysmem(ramdisk_addr, sizeof(*hdr));
+
+	kernel_size = hdr->kernel_size;
+	ramdisk_size = hdr->ramdisk_size;
+	page_size = hdr->page_size;
+	mmc_blksz = MMC_MAX_BLOCK_LEN;
+
+	/* hdr size : 1 page */
+	kernel_offset = bootimg_blk + (ALIGN(sizeof(struct andr_img_hdr), page_size) >> 9);
+	ramdisk_offset =  kernel_offset + (ALIGN(kernel_size, page_size) >> 9);
+
+	unmap_sysmem(hdr);
+
+	/* Step 2 : load rammdisk to address  */
+	if(ramdisk_size != 0) {
+		count = ALIGN(ramdisk_size, mmc_blksz) >> 9;
+
+		sprintf(command, "mmc read 0x%x 0x%x 0x%x",
+				ramdisk_addr, ramdisk_offset, count);
+		run_command(command, 0);
+
+		sprintf(command, "%x", ramdisk_size);
+		setenv("ramdisk_size", command);
+	}
+
+	return CMD_RET_SUCCESS;
+}
+
+
 #endif
 
-static cmd_tbl_t cmd_dtimg_sub[] = {
+static cmd_tbl_t cmd_aboot_sub[] = {
 	U_BOOT_CMD_MKENT(dump, 2, 0, do_aboot_dump, "", ""),
 #ifdef CONFIG_CMD_MMC
 	U_BOOT_CMD_MKENT(load_mmc, 4, 0, do_aboot_load_mmc, "", ""),
+	U_BOOT_CMD_MKENT(load_zImage, 3, 0, do_aboot_load_zImage, "", ""),
+	U_BOOT_CMD_MKENT(load_kernel, 3, 0, do_aboot_load_kernel, "", ""),
+	U_BOOT_CMD_MKENT(load_ramdisk, 3, 0, do_aboot_load_ramdisk, "", ""),
 #endif
 };
 
@@ -115,9 +251,9 @@ static int do_aboot(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	cmd_tbl_t *cp;
 
-	cp = find_cmd_tbl(argv[1], cmd_dtimg_sub, ARRAY_SIZE(cmd_dtimg_sub));
+	cp = find_cmd_tbl(argv[1], cmd_aboot_sub, ARRAY_SIZE(cmd_aboot_sub));
 
-	/* Strip off leading 'dtimg' command argument */
+	/* Strip off leading 'aboot' command argument */
 	argc--;
 	argv++;
 
@@ -139,5 +275,17 @@ U_BOOT_CMD(
 	"    - load from mmc from the image \n"
 	"      <boot.img offset>: blk offset of boot.img on mmc, in hex \n"
 	"      <kernel address>: kernel load RAM address \n"
+	"      <ramdisk address>: ramdisk load RAM address \n"
+	"aboot load_zImage <boot.img offset> <kernel address> \n"
+	"    - load from mmc from the zImage \n"
+	"      <boot.img offset>: blk offset of boot.img on mmc, in hex \n"
+	"      <kernel address>: kernel load RAM address \n"
+	"aboot load_kernel <boot.img offset> <kernel address> \n"
+	"    - load from mmc from the image \n"
+	"      <boot.img offset>: blk offset of boot.img on mmc, in hex \n"
+	"      <kernel address>: kernel load RAM address \n"
+	"aboot load_ramdisk <boot.img offset> <ramdisk address> \n"
+	"    - load from mmc from the image \n"
+	"      <boot.img offset>: blk offset of boot.img on mmc, in hex \n"
 	"      <ramdisk address>: ramdisk load RAM address "
 );
