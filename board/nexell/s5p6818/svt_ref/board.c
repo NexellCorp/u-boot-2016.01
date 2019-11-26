0641696e60d40bfdbe7f9006c4b7b4eec2b3b833
Lee Geunjeong <kjlee@nexell.co.kr>
2019-11-26 16:02:22 +0900

    board: Add s5p6818 svt_ref target
    This patch adds s5p6818 svt_ref target for the VTK board.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: I6f43d5af9c7b9f458584597769e279c370a68e87

---
+++ board/nexell/s5p6818/svt_ref/board.c
@@ -0,0 +1,124 @@
+/*
+ * (C) Copyright 2016 Nexell
+ * Hyejung Kwon  <cjscld15@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <config.h>
+#include <common.h>
+#include <asm/io.h>
+
+#include <asm/arch/nexell.h>
+#include <asm/arch/nx_gpio.h>
+
+#include <asm-generic/gpio.h>
+
+#include <nexell_common.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#ifdef CONFIG_AUTORECOVERY_CMD
+#include <nexell_recovery.h>
+#endif
+
+static void board_backlight_disable(void)
+{
+
+}
+
+static void board_backlight_enable(void)
+{
+
+}
+
+int board_init(void)
+{
+	board_backlight_disable();
+
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags |= GD_FLG_SILENT;
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_BOARD_LATE_INIT
+int board_late_init(void)
+{
+#ifdef CONFIG_SILENT_CONSOLE
+	gd->flags &= ~GD_FLG_SILENT;
+#endif
+
+	board_backlight_enable();
+
+#ifdef CONFIG_AUTORECOVERY_CMD
+	check_nexell_autorecovery();
+#endif
+
+#ifdef CONFIG_RECOVERY_BOOT
+#define ALIVE_SCRATCH1_READ_REGISTER	(0xc00108b4)
+#define ALIVE_SCRATCH1_RESET_REGISTER	(0xc00108ac)
+#define RECOVERY_SIGNATURE				(0x52455343)    /* (ASCII) : R.E.S.C */
+	printf("signature --> 0x%x\n", readl(ALIVE_SCRATCH1_READ_REGISTER));
+	if (readl(ALIVE_SCRATCH1_READ_REGISTER) == RECOVERY_SIGNATURE) {
+		printf("reboot recovery!!!!\n");
+		writel(0xffffffff, ALIVE_SCRATCH1_RESET_REGISTER);
+		setenv("bootcmd", "run recoveryboot");
+	}
+#endif
+
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_SPLASH_SOURCE
+#include <splash.h>
+struct splash_location splash_locations[] = {
+	{
+	.name = "mmc_fs",
+	.storage = SPLASH_STORAGE_MMC,
+	.flags = SPLASH_STORAGE_FS,
+	.devpart = "0:1",
+	},
+	{
+	.name = "mmc",
+	.storage = SPLASH_STORAGE_MMC,
+	.flags = SPLASH_STORAGE_RAW,
+	.offset = CONFIG_SPLASH_MMC_OFFSET,
+	},
+};
+
+int splash_screen_prepare(void)
+{
+	int err = splash_source_load(splash_locations,
+					sizeof(splash_locations)/sizeof(struct splash_location));
+	if (!err) {
+		char addr[64];
+
+		sprintf(addr, "0x%lx", gd->fb_base);
+		setenv("fb_addr", addr);
+	}
+
+	return err;
+}
+#endif
+
+/* u-boot dram initialize  */
+int dram_init(void)
+{
+	gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+	return 0;
+}
+
+/* u-boot dram board specific */
+void dram_init_banksize(void)
+{
+	/* set global data memory */
+	gd->bd->bi_arch_number = machine_arch_type;
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x00000100;
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size  = CONFIG_SYS_SDRAM_SIZE;
+}
+
