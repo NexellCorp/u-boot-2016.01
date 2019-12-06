e89d62688ae4e07b973206d30c5c886bb615a247
allan park <allan.park@nexell.co.kr>
2019-12-06 13:41:01 +0900

    u-boot: fix the libfdt.h, libfdt_env.h to avoid compile error
    This patch fix the libfdt.h, libfdt_env.h to avoid compile error.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: I56e9068ac04147aee95924e385c7eb59af2317a7
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2016.01/+/476977
    Tested-by: Nexell Jenkins <jenkins@nexell.co.kr>

--- include/libfdt_env.h
+++ include/libfdt_env.h
@@ -6,8 +6,8 @@
  * SPDX-License-Identifier:	LGPL-2.1+
  */
 
-#ifndef _LIBFDT_ENV_H
-#define _LIBFDT_ENV_H
+#ifndef LIBFDT_ENV_H
+#define LIBFDT_ENV_H
 
 #include "compiler.h"
 #include "linux/types.h"
